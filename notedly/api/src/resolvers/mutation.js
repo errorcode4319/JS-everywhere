3ea686a827cf1298c175f3d43193b2901923e153
errorcode4319 <sandlekang@gmail.com>
2021-10-30 13:31:18 +0900

    CRUD

---
+++ notedly/api/src/resolvers/mutation.js
@@ -0,0 +1,33 @@
+const models = require("../models")
+
+module.exports = {
+    newNote: async (parent, args, {models}) => {
+        return await models.Note.create({
+            content: args.content,
+            author: 'Adam Scoot'
+        });
+    },
+    deleteNote: async (parent, {id}, {models}) => {
+        try {
+            await models.Note.findOneAndRemove({_id: id});
+            return true;
+        } catch (err) {
+            return false;
+        }
+    },
+    updateNote: async (parent, {content, id}, {models}) => {
+        return await models.Note.findOneAndUpdate(
+            {
+                _id: id,
+            },
+            {
+                $set: {
+                    content
+                }
+            },
+            {
+                new: true
+            }
+        );
+    }
+}
