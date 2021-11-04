75c4aa428cd3940aa3a55de4c5a1c23bb39712a1
errorcode4319 <sandlekang@gmail.com>
2021-11-04 23:46:32 +0900

    dynamic query

--- notedly/web/src/pages/home.js
+++ notedly/web/src/pages/home.js
@@ -4,6 +4,7 @@ import ReactMarkdown from 'react-markdown';
 import {useQuery, gql} from '@apollo/client';
 
 import NoteFeed from '../components/NoteFeed';
+import Button from '../components/Button';
 
 const GET_NOTES = gql`
     query NoteFeed($cursor: String) {
@@ -32,7 +33,37 @@ const Home = () => {
 
     if (error) return <p>Error!</p>;
 
-    return <NoteFeed notes={data.noteFeed.notes} />;
+    return (
+        <React.Fragment>
+            <NoteFeed notes={data.noteFeed.notes} />
+            {data.noteFeed.hasNextPage && (
+                <Button
+                    onClick={() =>
+                        fetchMore({
+                            variables: {
+                                cursor: data.noteFeed.cursor 
+                            },
+                            updateQuery: (previousResult, {fetchMoreResult}) => {
+                                return {
+                                    noteFeed: {
+                                        cursor: fetchMoreResult.noteFeed.cursor,
+                                        hasNextPage: fetchMoreResult.noteFeed.hasNextPage,
+                                        notes: [
+                                            ...previousResult.noteFeed.notes,
+                                            ...fetchMoreResult.noteFeed.notes 
+                                        ],
+                                        __typename: 'noteFeed'
+                                    }
+                                };
+                            }
+                        })
+                    }
+                >
+                    Load more
+                </Button>
+            )}
+        </React.Fragment>
+    );
 };
 
 export default Home;
